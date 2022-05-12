6910ae9a834376de1ec8d2e7042ce335d50e096d
spirit160708 <103181350+spirit160708@users.noreply.github.com>
2022-05-12 11:11:34 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) < -100) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.Y) > 100) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
+})
